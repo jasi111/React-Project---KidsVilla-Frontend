d18438fea9f82e5c402f47b5749c4474aca81ae8
jasi111 <jasimaha@gmail.com>
2023-06-16 12:23:35 +0400

    Updates

---
+++ precache-manifest.2659ee7f40110b7e70974cb01d0d1c86.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81a2a1bea0b8b6cc203020a7284872e",
+    "url": "/kidsvilla-react-frontend/index.html"
+  },
+  {
+    "revision": "f77aa5a45c6f1476b0c2",
+    "url": "/kidsvilla-react-frontend/static/css/main.e15a61ed.chunk.css"
+  },
+  {
+    "revision": "efc740a60bb8acc6b14a",
+    "url": "/kidsvilla-react-frontend/static/js/0.521ef481.chunk.js"
+  },
+  {
+    "revision": "9827ceb52479a9379a75",
+    "url": "/kidsvilla-react-frontend/static/js/5.e96cf458.chunk.js"
+  },
+  {
+    "revision": "b9cefaffb3a5a4c73e0f",
+    "url": "/kidsvilla-react-frontend/static/js/About.b6105def.chunk.js"
+  },
+  {
+    "revision": "20199c78cd325db1a5a9",
+    "url": "/kidsvilla-react-frontend/static/js/Home.add1b7c7.chunk.js"
+  },
+  {
+    "revision": "f77aa5a45c6f1476b0c2",
+    "url": "/kidsvilla-react-frontend/static/js/main.a44b43a2.chunk.js"
+  },
+  {
+    "revision": "b9fc12b9028eac493c49",
+    "url": "/kidsvilla-react-frontend/static/js/runtime~main.b6379f1e.js"
+  }
+]);
